bc84e7ff876b2bdf6f10014f3b04ee2734ec0ae8
Stefan Hobohm <s.hobohm@tu-bs.de>
2013-05-10 15:28:41 +0200

    Task 3

---
+++ Task 3/src/client/Client.java
@@ -0,0 +1,322 @@
+package client;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.EOFException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import common.TextMessage;
+
+/**
+ * simple chat client
+ */
+public class Client implements Runnable {
+	public static boolean log = false;
+	public static boolean encryption = false;
+	public static boolean color = false;
+	public static boolean auth = false;
+	public static boolean authBoolSent = false;
+	public static boolean authBoolRec = false;
+	public int ID = (int) (Math.random()*10000);
+	
+	public static void main(String args[]) throws IOException {
+		if (args.length == 2) {
+			log = true;
+			encryption = true;
+			color = true;
+			auth = true;
+		} else if (args.length == 6){
+			if (Integer.parseInt(args[2]) == 1){
+				log = true;
+			}
+			if (Integer.parseInt(args[3]) == 1){
+				encryption = true;
+			}
+			if (Integer.parseInt(args[4]) == 1){
+				color = true;
+			}
+			if (Integer.parseInt(args[5]) == 1){
+				auth = true;
+			} else {
+				authBoolRec = true;
+			}
+		} else {
+			System.out.println("Syntax: ChatClient <host> <port>");
+			System.out.println("oder");
+			System.out.println("Syntax: ChatClient <host> <port> <log> <enryption> <color> <authentification>");
+			System.out.println("Wobei die Argumente 3-6 optional sind und durch '1' -> 'aktiviert' und '0' -> 'deaktiviert' definiert sind");
+			System.exit(0);
+		}
+		int mode = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Bitte wählen sie den Modus:\n[1] Konsole\n[2] GUI");
+		try{
+			mode = in.nextInt();
+		} catch (InputMismatchException ime) {
+			System.out.println("Bitte nur Zahlen eingeben. Exiting.");
+			System.exit(0);
+		}
+		in.close();
+		
+		switch (mode) {
+		case 1:
+			//TODO
+			break;
+		case 2:
+			Client client = new Client(args[0], Integer.parseInt(args[1]));
+			new Gui("Chat " + args[0] + ":" + args[1], client);
+			break;
+		default:
+			System.out.println("Ungültige Eingabe. Exiting.");
+			break;
+		}
+	}
+
+
+	
+	protected ObjectInputStream inputStream;
+
+	protected ObjectOutputStream outputStream;
+
+	protected Thread thread;
+
+	public Client(String host, int port) {
+		try {
+			System.out.println("Connecting to " + host + " (port " + port + ")...");
+			Socket s = new Socket(host, port);
+			this.outputStream = new ObjectOutputStream((s.getOutputStream()));
+			this.inputStream = new ObjectInputStream((s.getInputStream()));
+			thread = new Thread(this);
+			thread.start();
+		} catch (ConnectException ce) {
+			System.out.println("Could not connect. Is the Server running?");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * main method. waits for incoming messages.
+	 */
+	public void run() {
+		try {
+			Thread thisthread = Thread.currentThread();
+			while (thread == thisthread) {
+				try {
+					Object msg = inputStream.readObject();
+					handleIncomingMessage(msg);
+					if (authBoolSent != true){
+						if (auth){
+							sendAuthRequest();
+						}
+						authBoolSent = true;
+					}
+				} catch (EOFException e) {
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			thread = null;
+			try {
+				outputStream.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	
+	public static boolean getLogBool(){
+		return log;
+	}
+	
+	public static boolean getAuthBool(){
+		return auth;
+	}
+	
+	public static boolean getEncBool(){
+		return encryption;
+	}
+	
+	public static boolean getColorBool(){
+		return color;
+	}
+	
+	/**
+	 * decides what to do with incoming messages
+	 * 
+	 * @param msg
+	 *            the message (Object) received from the sockets
+	 */
+	protected void handleIncomingMessage(Object msg) {
+		if (msg instanceof TextMessage) {
+			String text = decrypt(((TextMessage) msg).getContent());
+			if (text.matches("(accepted,)(\\d*)")){
+				String[] splitted = text.split(",");
+				Integer recvID = Integer.parseInt(splitted[1]);
+				if (recvID == ID) {
+					authBoolRec = true;
+					System.out.println("Authentification successful. Your ID is " + ID);
+				}
+			} else {
+				fireAddLine(decrypt(((TextMessage) msg).getContent()) + "\n");
+			}
+		}
+	}
+
+	public void send(String line) {
+		if (authBoolRec){
+			send(new TextMessage(encrypt(line)));
+		}else{
+			System.out.println("No auth-token received. You can not send messages.");
+		}
+	}
+
+	public void sendAuthRequest(){
+		send(new TextMessage(encrypt("password," + ID)));
+	}
+	
+	public void send(TextMessage msg) {
+		try {
+			outputStream.writeObject(msg);
+			outputStream.flush();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			this.stop();
+		}
+	}
+
+	/**
+	 * listener-list for the observer pattern
+	 */
+	private ArrayList listeners = new ArrayList();
+
+	/**
+	 * addListner method for the observer pattern
+	 */
+	public void addLineListener(ChatLineListener listener) {
+		listeners.add(listener);
+	}
+
+	/**
+	 * removeListner method for the observer pattern
+	 */
+	public void removeLineListener(ChatLineListener listner) {
+		listeners.remove(listner);
+	}
+
+	/**
+	 * fire Listner method for the observer pattern
+	 */
+	public void fireAddLine(String line) {
+		for (Iterator iterator = listeners.iterator(); iterator.hasNext();) {
+			ChatLineListener listener = (ChatLineListener) iterator.next();
+			listener.newChatLine(line);
+			logger(line);
+		}
+	}
+
+	public void stop() {
+		thread = null;
+	}
+	
+	/**
+	 * Methode zum verschlüsseln der Nachrichten
+	 * @param in verschlüsselter String
+	 * @return entschlüsslter String
+	 */
+	public String decrypt(String in) {
+		if (encryption){
+			char[] chars = in.toCharArray();
+			int j = chars.length;
+			char[] chars2 = new char[j];
+			char[] chars3 = new char[j];
+			//Text umdrehen
+			for (int i = 0; i <= j-1; i++){
+				chars2[j-1-i] = chars[i];
+			}
+			
+			//Rot13
+			String alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+			int x = alphabet.length();
+			for (int i = 0; i <= j-1; i++){
+				for (int y = 0; y < x; y++){
+					//System.out.println("[" + alphabet.charAt(y) + "] [" + chars[i] + "]");
+					if (alphabet.charAt(y) == chars2[i]){
+						chars3[i] = alphabet.charAt((y+x-13)%x);
+						break;
+					}
+				}
+			}
+			return new String(chars3);
+		} else {
+			return in;
+		}
+		
+		
+	}
+
+	/**
+	 * Methode zum entschlüsseln der Nachrichten
+	 * @param in String
+	 * @return verschlüsslter String
+	 */
+	public String encrypt(String in){
+		if (encryption) {
+			char[] chars = in.toCharArray();
+			int j = chars.length;
+			char[] chars2 = new char[j];
+			char[] chars3 = new char[j];
+			
+			//Rot13
+			String alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+			int x = alphabet.length();
+			for (int i = 0; i <= j-1; i++){
+				for (int y = 0; y < x; y++){
+					//System.out.println("[" + alphabet.charAt(y) + "] [" + chars[i] + "]");
+					if (alphabet.charAt(y) == chars[i]){
+						chars2[i] = alphabet.charAt((y+x+13)%x);
+						break;
+					}
+				}
+			}
+			
+			
+			//Text umdrehen
+			for (int i = 0; i <= j-1; i++){
+				chars3[j-1-i] = chars2[i];
+			}
+			return new String(chars3);
+		} else {
+			return in;
+		}
+	}
+	
+	public void logger(String text){
+		if (log){
+			try {
+			    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("log_client_" + ID + ".txt", true)));
+			    out.println(text);
+			    out.close();
+			} catch (IOException e) {
+	
+			}
+		}
+	}
+	
+
+}
---
+++ Task 3/src/client/Gui.java
@@ -0,0 +1,196 @@
+package client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.GroupLayout;
+import client.*;
+
+
+/**
+ * simple swing gui for the chat client
+ */
+public class Gui extends JFrame implements ChatLineListener {
+
+	private static final long serialVersionUID = 1L;
+
+	protected JTextArea outputTextbox;
+	protected JTextField inputField;
+
+	private static int rowstextarea = 20;
+	private static int colstextarea = 60;
+
+	private Client chatClient;
+	
+	private String farbe = "[schwarz] | ";
+	
+	private String fontItalic = "";
+	private String fontBold = "";
+	
+	
+	
+	/**
+	 * creates layout
+	 * 
+	 * @param title
+	 *            title of the window
+	 * @param chatClient
+	 *            chatClient that is used for sending and receiving messages
+	 */
+	public Gui(String title, Client chatClient) {
+		System.out.println("starting gui...");
+		outputTextbox = new JTextArea(Gui.rowstextarea, Gui.colstextarea);
+					outputTextbox.setEditable(false);
+					inputField = new JTextField();
+					inputField.addActionListener(getInput());
+		/*if [Color]*/if (Client.getColorBool()) {
+			farbe = "[schwarz] ";
+			
+			// Creates a menubar for a JFrame
+	        JMenuBar menuBar = new JMenuBar();
+	        
+	        // Add the menubar to the frame
+	        setJMenuBar(menuBar);
+	        
+	        // Define and add two drop down menu to the menubar
+	        JMenu fileMenu = new JMenu("Datei");
+	        JMenu colorMenu = new JMenu("Farbe");
+	        JMenu fontMenu = new JMenu("Schriftart");
+	        menuBar.add(fileMenu);
+	        menuBar.add(colorMenu);
+	        menuBar.add(fontMenu);
+	
+	
+			ButtonGroup colorGroup = new ButtonGroup();
+			ButtonGroup fontGroup = new ButtonGroup();
+	
+			JRadioButtonMenuItem colorActionBlack = new JRadioButtonMenuItem("schwarz");
+	        colorMenu.add(colorActionBlack);
+	        colorGroup.add(colorActionBlack);
+			
+	        JRadioButtonMenuItem colorActionRed = new JRadioButtonMenuItem("rot");
+	        colorMenu.add(colorActionRed);
+	        colorGroup.add(colorActionRed);
+	        
+	        JRadioButtonMenuItem colorActionBlue = new JRadioButtonMenuItem("blau");
+	        colorMenu.add(colorActionBlue);
+	        colorGroup.add(colorActionBlue);
+	        
+	        JRadioButtonMenuItem colorActionGreen = new JRadioButtonMenuItem("grün");
+	        colorMenu.add(colorActionGreen);
+	        colorGroup.add(colorActionGreen);
+	        
+	        colorActionBlack.setSelected(true);
+	        
+	        JCheckBoxMenuItem fontActionItalic = new JCheckBoxMenuItem("italic");
+	        fontMenu.add(fontActionItalic);
+	        fontGroup.add(fontActionItalic);
+	        
+	        JCheckBoxMenuItem fontActionBold = new JCheckBoxMenuItem("bold");
+	        fontMenu.add(fontActionBold);
+	        fontGroup.add(fontActionBold);
+	
+			
+			
+			
+			
+			
+			
+			colorActionRed.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent arg0) {
+	                farbe = "[rot] ";
+	            }
+	        });
+			
+			colorActionBlack.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent arg0) {
+	                farbe = "[schwarz] ";
+	            }
+	        });
+			
+			colorActionBlue.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent arg0) {
+	                farbe = "[blau] ";
+	            }
+	        });
+			
+			colorActionGreen.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent arg0) {
+	                farbe = "[grün] ";
+	            }
+	        });
+			
+			
+			fontActionItalic.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent arg0) {
+	            	fontItalic = "(italic) ";
+	            }
+	        });
+			
+			fontActionBold.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent arg0) {
+	            	fontItalic = "(bold) ";
+	            }
+	        });
+		} else {
+			farbe = "";
+		}
+		/*end [Color]*/
+		GroupLayout layout = new GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(outputTextbox)
+                    .addComponent(inputField))
+        );
+
+        // layout.linkSize(SwingConstants.HORIZONTAL, findButton, cancelButton);
+
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                    .addComponent(outputTextbox)
+                    .addComponent(inputField));
+		
+    	// register listener so that we are informed whenever a new chat message
+		// is received (observer pattern)
+		chatClient.addLineListener(this);
+
+		setTitle(title);
+		pack();
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.chatClient = chatClient;
+	}
+	
+	
+	
+	private ActionListener getInput() {
+		return new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chatClient.send((String) farbe + fontItalic + fontBold + "| " + inputField.getText());
+				inputField.setText("");
+			}
+		};
+	}
+
+	/**
+	 * this method gets called every time a new message is received (observer
+	 * pattern)
+	 */
+	public void newChatLine(String line) {
+		outputTextbox.append(line);
+	}
+
+}
